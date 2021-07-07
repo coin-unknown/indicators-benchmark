b10f4a82bb5e31c0adc87b11eabae6a25688b5a9
Yurov Dmitry <dmitry.yurov@corp.mail.ru>
2021-07-07 13:08:10 +0300

    update bench

--- suites/ema.js
+++ suites/ema.js
@@ -1,6 +1,8 @@
 import Benchmark from 'benchmark';
 import { EMA as EMA1 } from 'technicalindicators';
 import { EMA as EMA2 } from '@debut/indicators';
+import { EMA as EMA3 } from 'trading-signals';
+import ta from 'ta.js';
 
 const DATA_LENGTH = 1000;
 const PERIOD = 12;
@@ -9,7 +11,7 @@ const suite = new Benchmark.Suite();
 const dataset = Array.from({ length: DATA_LENGTH }, () => Math.random() * 40);
 const ema1 = new EMA1({ period: PERIOD, values: [] });
 const ema2 = new EMA2(PERIOD);
-
+const ema3 = new EMA3(PERIOD);
 // technicalindicators SMA x 4,721 ops/sec
 // @debut/indicators SMA x 62,511 ops/sec
 
@@ -19,6 +21,14 @@ suite
             ema1.nextValue(dataset[i]);
         }
     })
+    .add('trading-signals EMA', function () {
+        for (let i = 0; i < DATA_LENGTH; i++) {
+            ema3.update(dataset[i]);
+        }
+    })
+    .add('ta.js EMA', function () {
+        ta.ema(dataset, PERIOD);
+    })
     .add('@debut/indicators EMA', function () {
         for (let i = 0; i < DATA_LENGTH; i++) {
             ema2.nextValue(dataset[i]);
@@ -27,4 +37,4 @@ suite
     .on('cycle', function (event) {
         console.log(String(event.target));
     })
-    .run({ async: true });
+    .run();
